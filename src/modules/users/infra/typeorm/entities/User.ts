18d9fcdab6c1acb9632907b32b19713f829d4b55
Luismkm <luismarcelo13@yahoo.com.br>
2021-05-22 08:45:05 -0300

    first commit

---
+++ src/modules/users/infra/typeorm/entities/User.ts
@@ -0,0 +1,31 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity('users')
+export default class Users {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  surname: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  password: string;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+}
