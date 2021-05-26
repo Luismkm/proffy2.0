8ccae545b78e236f30707b1d890be55a2f8b1e86
Luismkm <luismarcelo13@yahoo.com.br>
2021-05-25 23:01:36 -0300

    add teacher and schedule repository

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -5,7 +5,23 @@ import '@modules/users/providers';
 import IUsersRepository from '@modules/users/repositories/IUsersRepository';
 import UsersRepository from '@modules/users/infra/typeorm/repositories/UsersRepository';
 
+import ITeachersRepository from '@modules/teachers/repositories/ITeachersRepository';
+import TeachersRepository from '@modules/teachers/infra/typeorm/repositories/TeacherRepository';
+
+import ISchedulesRepository from '@modules/schedules/repositories/ISchedulesRepository';
+import SchedulesRepository from '@modules/schedules/infra/typeorm/repositories/SchedulesRepository';
+
 container.registerSingleton<IUsersRepository>(
   'UsersRepository',
   UsersRepository,
+);
+
+container.registerSingleton<ITeachersRepository>(
+  'TeachersRepository',
+  TeachersRepository,
+);
+
+container.registerSingleton<ISchedulesRepository>(
+  'SchedulesRepository',
+  SchedulesRepository,
 );
