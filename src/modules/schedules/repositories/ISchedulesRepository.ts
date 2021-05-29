becb8863f17bc4b2b39d02628fe02c380a6d9f91
Luismkm <luismarcelo13@yahoo.com.br>
2021-05-28 22:14:51 -0300

    interface function findById and save

--- src/modules/schedules/repositories/ISchedulesRepository.ts
+++ src/modules/schedules/repositories/ISchedulesRepository.ts
@@ -3,4 +3,6 @@ import Schedule from '../infra/typeorm/entities/Schedule';
 
 export default interface ISchedulesRepository {
   create(data: ICreateScheduleDTO): Promise<Schedule[]>;
+  findById(id: string): Promise<Schedule[] | undefined>;
+  save(schedule: ICreateScheduleDTO): Promise<Schedule[]>;
 }
