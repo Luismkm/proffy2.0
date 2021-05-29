aea64a47368a68506a72c7a4d1d3ddbb8549e4f5
Luismkm <luismarcelo13@yahoo.com.br>
2021-05-28 22:59:34 -0300

    add profileRouter

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -3,11 +3,13 @@ import { Router } from 'express';
 import usersRouter from '@modules/users/infra/http/routes/users.routes';
 import sessionsRouter from '@modules/users/infra/http/routes/sessions.routes';
 import teachersRouter from '@modules/teachers/infra/http/routes/teachers.routes';
+import profileRouter from '@modules/teachers/infra/http/routes/profile.routes';
 
 const routes = Router();
 
 routes.use('/users', usersRouter);
 routes.use('/sessions', sessionsRouter);
 routes.use('/teachers', teachersRouter);
+routes.use('/profile', profileRouter);
 
 export default routes;
