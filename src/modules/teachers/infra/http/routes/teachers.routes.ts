1c0dfb4996827232fc68dc9d940cbdf71e7e93cc
Luismkm <luismarcelo13@yahoo.com.br>
2021-05-29 19:43:51 -0300

    add authentication middleware

--- src/modules/teachers/infra/http/routes/teachers.routes.ts
+++ src/modules/teachers/infra/http/routes/teachers.routes.ts
@@ -2,6 +2,8 @@ import { Router } from 'express';
 import multer from 'multer';
 import uploadConfig from '@config/upload';
 
+import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
+
 import TeachersController from '../controllers/TeachersController';
 import TeacherAvatarController from '../controllers/TeacherAvatarController';
 
@@ -10,6 +12,8 @@ const teachersController = new TeachersController();
 const teacherAvatarController = new TeacherAvatarController();
 
 const upload = multer(uploadConfig.multer);
+
+teachersRouter.use(ensureAuthenticated);
 
 teachersRouter.post('/', teachersController.create);
 teachersRouter.patch(
