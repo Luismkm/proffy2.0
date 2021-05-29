9a09ec6aea1599932f35d449831c6e39b0d46a7b
Luismkm <luismarcelo13@yahoo.com.br>
2021-05-29 19:50:38 -0300

    add upload file middleware

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -3,6 +3,8 @@ import 'reflect-metadata';
 import express, { Request, Response, NextFunction } from 'express';
 import 'express-async-errors';
 
+import uploadConfig from '@config/upload';
+
 import AppError from '@shared/errors/AppError';
 import routes from './routes';
 
@@ -12,9 +14,10 @@ import '@shared/container';
 const app = express();
 
 app.use(express.json());
+app.use('/files', express.static(uploadConfig.uploadsFolder));
 app.use(routes);
 
-app.use(
+/* app.use(
   (err: Error, request: Request, response: Response, next: NextFunction) => {
     if (err instanceof AppError) {
       return response.status(err.statusCode).json({
@@ -28,7 +31,7 @@ app.use(
       message: 'Internal server error',
     });
   },
-);
+); */
 
 app.listen(3333, () => {
   console.log('Server started on port 3333');
