fbfc96a8b072a61be4517463d2e306eea95670d7
Luismkm <luismarcelo13@yahoo.com.br>
2021-05-28 22:49:59 -0300

    add route update avatar

--- src/modules/teachers/infra/http/routes/teachers.routes.ts
+++ src/modules/teachers/infra/http/routes/teachers.routes.ts
@@ -1,10 +1,21 @@
 import { Router } from 'express';
+import multer from 'multer';
+import uploadConfig from '@config/upload';
 
 import TeachersController from '../controllers/TeachersController';
+import TeacherAvatarController from '../controllers/TeacherAvatarController';
 
 const teachersRouter = Router();
 const teachersController = new TeachersController();
+const teacherAvatarController = new TeacherAvatarController();
+
+const upload = multer(uploadConfig.multer);
 
 teachersRouter.post('/', teachersController.create);
+teachersRouter.patch(
+  '/avatar',
+  upload.single('avatar'),
+  teacherAvatarController.update,
+);
 
 export default teachersRouter;
