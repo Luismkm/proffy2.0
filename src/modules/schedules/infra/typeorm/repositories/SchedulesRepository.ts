1778282a5679e9c15c060dfc3342d7056b10b7f2
Luismkm <luismarcelo13@yahoo.com.br>
2021-05-29 19:38:00 -0300

    add relations with weekday in function findById

--- src/modules/schedules/infra/typeorm/repositories/SchedulesRepository.ts
+++ src/modules/schedules/infra/typeorm/repositories/SchedulesRepository.ts
@@ -25,6 +25,7 @@ export default class SchedulesRepository implements ISchedulesRepository {
       where: {
         teacher_id: id,
       },
+      relations: ['weekday'],
     });
 
     return schedule;
