3302f03f9fa7a99b164f1229d34d6154bd680b20
Luismkm <luismarcelo13@yahoo.com.br>
2021-05-31 21:55:58 -0300

    adding new route /password

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -2,6 +2,7 @@ import { Router } from 'express';
 
 import usersRouter from '@modules/users/infra/http/routes/users.routes';
 import sessionsRouter from '@modules/users/infra/http/routes/sessions.routes';
+import passwordRouter from '@modules/users/infra/http/routes/password.routes';
 import teachersRouter from '@modules/teachers/infra/http/routes/teachers.routes';
 import profileRouter from '@modules/teachers/infra/http/routes/profile.routes';
 
@@ -11,5 +12,6 @@ routes.use('/users', usersRouter);
 routes.use('/sessions', sessionsRouter);
 routes.use('/teachers', teachersRouter);
 routes.use('/profile', profileRouter);
+routes.use('/password', passwordRouter);
 
 export default routes;
