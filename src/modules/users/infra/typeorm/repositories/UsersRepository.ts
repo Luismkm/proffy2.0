10985b214480fc2771de9b110ff3c86687f9fe20
Luismkm <luismarcelo13@yahoo.com.br>
2021-05-31 21:48:29 -0300

    adding function findById and save

--- src/modules/users/infra/typeorm/repositories/UsersRepository.ts
+++ src/modules/users/infra/typeorm/repositories/UsersRepository.ts
@@ -20,11 +20,21 @@ export default class UsersRepository implements IUsersRepository {
     return user;
   }
 
+  public async findById(id: string): Promise<User | undefined> {
+    const user = await this.ormRepository.findOne(id);
+
+    return user;
+  }
+
   public async findByEmail(email: string): Promise<User | undefined> {
     const user = await this.ormRepository.findOne({
       where: { email },
     });
 
     return user;
+  }
+
+  public async save(user: User): Promise<User> {
+    return this.ormRepository.save(user);
   }
 }
