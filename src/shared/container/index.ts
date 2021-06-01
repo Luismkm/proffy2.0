a356fa2f84e75ca1f8413a2404457932eb1f6e7a
Luismkm <luismarcelo13@yahoo.com.br>
2021-05-31 21:52:56 -0300

    adding dependency injection to UserTokensRepository

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -6,6 +6,9 @@ import './providers';
 import IUsersRepository from '@modules/users/repositories/IUsersRepository';
 import UsersRepository from '@modules/users/infra/typeorm/repositories/UsersRepository';
 
+import IUserTokensRepository from '@modules/users/repositories/IUserTokensRepository';
+import UserTokensRepository from '@modules/users/infra/typeorm/repositories/UserTokensRepository';
+
 import ITeachersRepository from '@modules/teachers/repositories/ITeachersRepository';
 import TeachersRepository from '@modules/teachers/infra/typeorm/repositories/TeacherRepository';
 
@@ -15,6 +18,11 @@ import SchedulesRepository from '@modules/schedules/infra/typeorm/repositories/S
 container.registerSingleton<IUsersRepository>(
   'UsersRepository',
   UsersRepository,
+);
+
+container.registerSingleton<IUserTokensRepository>(
+  'UserTokensRepository',
+  UserTokensRepository,
 );
 
 container.registerSingleton<ITeachersRepository>(
